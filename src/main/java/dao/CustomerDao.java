266ac2bbe9d4ee884820183bc5e5d2c80550b803
Suchang Cao <caosuchang@yeah.net>
2019-05-03 17:53:29 -0400

    Remove customer opition to signup

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -33,14 +33,14 @@ public class CustomerDao {
     }
     public List<Customer> getDummyCustomerList() {
         /*Sample data begins*/
-        List<Customer> customers = new ArrayList<Customer>();
+//        List<Customer> customers = new ArrayList<Customer>();
 
-        for (int i = 0; i < 10; i++) {
-            customers.add(getDummyCustomer());
-        }
+//        for (int i = 0; i < 10; i++) {
+//            customers.add(getCustomer());
+//        }
 		/*Sample data ends*/
 
-        return customers;
+        return getAllCustomers();
     }
 
     /**
@@ -57,8 +57,58 @@ public class CustomerDao {
 		 * The students code to fetch data from the database based on searchKeyword will be written here
 		 * Each record is required to be encapsulated as a "Customer" class object and added to the "customers" List
 		 */
-		
-		return getDummyCustomerList();
+
+		List<Customer> customers = new ArrayList<Customer>();
+		//connections
+		Connection connection = null;
+		Statement statement = null;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://107.155.113.86:3306/STOCKSYSTEM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"cse305","CSE305XYZ");
+			connection.setAutoCommit(false); // only one transaction
+			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT Id FROM Client");
+			while (resultSet.next()){
+				//construct customer
+				Customer customer = getCustomer(resultSet.getInt("Id")+"");
+				if (customer!=null){
+					customers.add(customer);
+				}
+			}
+			//clean up
+			connection.commit();
+			resultSet.close();
+			statement.close();
+			connection.close();
+
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+			try{
+				if (connection!=null)
+					connection.rollback();
+			}catch (Exception e){
+				System.out.println(e.getMessage());
+			}
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}finally {
+			try{
+				if (statement!=null)
+					statement.close();
+			}catch (SQLException se2){
+				System.out.println(se2.getMessage());
+			}
+			try{
+				if (connection!=null)
+					connection.close();
+			}catch (SQLException se3){
+				System.out.println(se3.getMessage());
+			}
+		}
+
+		return customers;
 	}
 
 
@@ -80,8 +130,91 @@ public class CustomerDao {
 		 * The students code to fetch data from the database will be written here
 		 * The customer record is required to be encapsulated as a "Customer" class object
 		 */
-		
-		return getDummyCustomer();
+		System.out.println("get customer begin!!");
+		Connection connection = null;
+		Statement statement = null;
+		Customer customer = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://107.155.113.86:3306/STOCKSYSTEM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"cse305", "CSE305XYZ");
+			connection.setAutoCommit(false); // only one transaction
+			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			statement = connection.createStatement();
+			System.out.println(customerID);
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM Client C WHERE C.Id = "+Integer.parseInt(customerID));
+			System.out.println("TEST MESSAGE7");
+			while (resultSet.next()){
+				customer = new Customer();
+				customer.setClientId(resultSet.getInt("Id")+"");
+//				customer.setSsn(resultSet.getInt("SSN")+"");
+				customer.setCreditCard(resultSet.getString("CreditCardNumber"));
+				customer.setRating(resultSet.getInt("Rating"));
+//				customer.setEmail(resultSet.getString("Email"));
+
+				//find person
+
+				ResultSet findCustomer = connection.createStatement().executeQuery(
+						"SELECT * FROM Person P WHERE P.SSN = "+Integer.parseInt(resultSet.getString("Id")));
+
+				while (findCustomer.next()){
+					customer.setFirstName(findCustomer.getString("FirstName"));
+					customer.setLastName(findCustomer.getString("LastName"));
+					customer.setAddress(findCustomer.getString("Address"));
+					customer.setEmail(findCustomer.getString("Email"));
+					customer.setTelephone(findCustomer.getLong("Telephone")+"");
+					//find location
+					ResultSet findLocation = connection.createStatement().executeQuery("SELECT * FROM Location L WHERE L.ZipCode = "
+							+findCustomer.getInt("ZipCode"));
+					while (findLocation.next()){
+						Location location = new Location();
+						location.setCity(findLocation.getString("City"));
+						location.setState(findLocation.getString("State"));
+						location.setZipCode(findLocation.getInt("ZipCode"));
+						customer.setLocation(location);
+					}
+					findLocation.close();
+				}
+				findCustomer.close();
+			}
+			//clean up
+			connection.commit();
+
+			resultSet.close();
+			statement.close();
+			connection.close();
+			return customer;
+
+		}catch(SQLException ex){
+			System.out.println("SQL EXCEPTION NOW, EDITING!!!");
+			System.out.println(ex.getMessage());
+			try{
+				if (connection!=null)
+					connection.rollback();
+			}catch (Exception e){
+				System.out.println("SQL EXCEPTION NOW2, EDITING!!!");
+				System.out.println(e.getMessage());
+			}
+		} catch (Exception e){
+			System.out.println("SQL EXCEPTION NOW3, EDITING!!!");
+			System.out.println(e.getMessage());
+		}finally {
+			try{
+				if (statement!=null)
+					statement.close();
+			}catch (SQLException se2){
+				System.out.println("SQL EXCEPTION NOW4, EDITING!!!");
+				System.out.println(se2.getMessage());
+			}
+			try{
+				if (connection!=null)
+					connection.close();
+			}catch (SQLException se3){
+				System.out.println("SQL EXCEPTION NOW5, EDITING!!!");
+				System.out.println(se3.getMessage());
+			}
+		}
+		return customer;
 	}
 	
 	public String deleteCustomer(String customerID) {
@@ -93,6 +226,7 @@ public class CustomerDao {
 		 */
 
 		/*Sample data begins*/
+		System.out.println("shan chu customer");
 		return "success";
 		/*Sample data ends*/
 		
@@ -106,7 +240,7 @@ public class CustomerDao {
 		 * username, which is the email address of the customer, who's ID has to be returned, is given as method parameter
 		 * The Customer's ID is required to be returned as a String
 		 */
-
+		System.out.println("de dao ID customer");
 		return "111-11-1111";
 	}
 
@@ -120,7 +254,7 @@ public class CustomerDao {
 		 * The sample code returns "success" by default.
 		 * You need to handle the database insertion of the customer details and return "success" or "failure" based on result of the database insertion.
 		 */
-
+		System.out.println("jia yi ge customer");
 		Connection connection = null;
 		Statement statement = null;
 		PreparedStatement preparedStatement = null;
@@ -137,7 +271,7 @@ public class CustomerDao {
 			ResultSet resultSet = statement.executeQuery(
 					"SELECT C.ID FROM Client C WHERE C.ID = "+Integer.parseInt(customer.getSsn()));
 			while (resultSet.next()){
-				tempSSN = resultSet.getInt("SSN");
+				tempSSN = resultSet.getInt("Id");
 			}
 			//fail to insert
 			if (tempSSN!=-1){
@@ -192,31 +326,41 @@ public class CustomerDao {
 			preparedStatement.close();
 			connection.close();
 			return "success";
-		}catch(SQLException se){
-			se.printStackTrace();
-			return "fail";
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+			try{
+				if (connection!=null)
+					connection.rollback();
+			}catch (Exception e){
+				System.out.println(e.getMessage());
+			}
 		}catch(Exception e) {
 			//Handle errors for Class.forName
 			e.printStackTrace();
-			return "fail";
-		}finally{
+
+		}finally {
 			try{
-				if(statement!=null)
-					connection.close();
-			}catch(SQLException se){
-				//我tm怎么知道要干嘛
+				if (statement!=null)
+					statement.close();
+			}catch (SQLException se2){
+				System.out.println(se2.getMessage());
 			}
 			try{
-				if(connection!=null)
-					connection.close();
-			}catch(SQLException se){
-				se.printStackTrace();
+				if (preparedStatement!=null)
+					preparedStatement.close();
+			}catch (SQLException s2){
+				System.out.println(s2.getMessage());
 			}
-			return "success";
+			try{
+				if (connection!=null)
+					connection.close();
+			}catch (SQLException se3){
+				System.out.println(se3.getMessage());
+			}
 		}
 
 		/*Sample data begins*/
-//		return "success";
+	return "failure";
 		/*Sample data ends*/
 
 	}
@@ -231,7 +375,111 @@ public class CustomerDao {
 		 */
 		
 		/*Sample data begins*/
-		return "success";
+		System.out.println("NOW, EDITING!!!");
+		Connection connection = null;
+		Statement statement = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://107.155.113.86:3306/STOCKSYSTEM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"cse305", "CSE305XYZ");
+			connection.setAutoCommit(false); // only one transaction
+			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			statement = connection.createStatement();
+			int tempSSN = -1;
+			//check client table
+			ResultSet resultSet = statement.executeQuery(
+					"SELECT Id FROM Client C WHERE C.Id = "+customer.getId());
+			while (resultSet.next()){
+				tempSSN = resultSet.getInt("Id");
+			}
+			if (tempSSN == -1)
+				return "failure";
+			//update client table
+			preparedStatement = connection.prepareStatement(
+					"UPDATE Client C SET C.Email = ?, C.Rating = ?, C.CreditCardNumber= ? " +
+							"WHERE C.Id = "+Integer.parseInt(customer.getClientId()));
+			preparedStatement.setString(1,customer.getEmail());
+			preparedStatement.setInt(2,customer.getRating());
+			preparedStatement.setString(3,customer.getCreditCard());
+			preparedStatement.executeUpdate();
+			//update location table
+			int tempZip = -1;
+			resultSet = statement.executeQuery(
+					"SELECT ZipCode FROM Location L WHERE L.ZipCode = "+customer.getLocation().getZipCode());
+			while (resultSet.next()){
+				tempZip = resultSet.getInt("ZipCode");
+			}
+			//New location then add
+			if (tempZip==-1){
+				preparedStatement = connection.prepareStatement(
+						"INSERT INTO Location(ZipCode, City, State) VALUE (?,?,?)");
+				preparedStatement.setInt(1,customer.getLocation().getZipCode());
+				preparedStatement.setString(2,customer.getLocation().getCity());
+				preparedStatement.setString(3,customer.getLocation().getState());
+				preparedStatement.executeUpdate();
+			}
+			//update person table
+			preparedStatement = connection.prepareStatement(
+					"UPDATE Person P " +
+							"SET P.FirstName =?,P.LastName =?,P.Address =?,P.ZipCode =?,P.Email = ?,P.Telephone =? " +
+							"WHERE P.SSN = "+Integer.parseInt(customer.getSsn()));
+			preparedStatement.setString(1,customer.getFirstName());
+			preparedStatement.setString(2,customer.getLastName());
+			preparedStatement.setString(3,customer.getAddress());
+			preparedStatement.setInt(4,customer.getLocation().getZipCode());
+			preparedStatement.setString(5,customer.getEmail());
+			preparedStatement.setLong(6,Long.parseLong(customer.getTelephone()));
+			preparedStatement.executeUpdate();
+			//update login table
+			preparedStatement = connection.prepareStatement("UPDATE Login L SET L.Role =? " +
+					"WHERE L.Email = '"+customer.getEmail()+"'");
+//			preparedStatement.setString(1,customer.getRating());
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+			resultSet.close();
+			preparedStatement.close();
+			statement.close();
+			connection.close();
+			return "success";
+
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+			try{
+				if (connection!=null)
+					connection.rollback();
+			}catch (Exception e){
+				System.out.println(e.getMessage());
+			}
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}finally {
+			try{
+				if (statement!=null)
+					statement.close();
+			}catch (SQLException se2){
+				System.out.println(se2.getMessage());
+			}
+			try{
+				if (preparedStatement!=null)
+					preparedStatement.close();
+			}catch (SQLException s2){
+				System.out.println(s2.getMessage());
+			}
+			try{
+				if (connection!=null)
+					connection.close();
+			}catch (SQLException se3){
+				System.out.println(se3.getMessage());
+			}
+		}
+		return "failure";
+		/*Sample data ends*/
+
+
+
 		/*Sample data ends*/
 
 	}
@@ -250,6 +498,57 @@ public class CustomerDao {
         /*
 		 * This method fetches returns all customers
 		 */
-        return getDummyCustomerList();
+		System.out.println("suo you customer");
+		List<Customer> customers = new ArrayList<Customer>();
+		//connections
+		Connection connection = null;
+		Statement statement = null;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://107.155.113.86:3306/STOCKSYSTEM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"cse305","CSE305XYZ");
+			connection.setAutoCommit(false); // only one transaction
+			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT Id FROM Client");
+			while (resultSet.next()){
+				//construct customer
+				Customer customer = getCustomer(resultSet.getInt("Id")+"");
+				if (customer!=null){
+					customers.add(customer);
+				}
+			}
+			//clean up
+			connection.commit();
+			resultSet.close();
+			statement.close();
+			connection.close();
+
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+			try{
+				if (connection!=null)
+					connection.rollback();
+			}catch (Exception e){
+				System.out.println(e.getMessage());
+			}
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}finally {
+			try{
+				if (statement!=null)
+					statement.close();
+			}catch (SQLException se2){
+				System.out.println(se2.getMessage());
+			}
+			try{
+				if (connection!=null)
+					connection.close();
+			}catch (SQLException se3){
+				System.out.println(se3.getMessage());
+			}
+		}
+
+		return customers;
     }
 }
